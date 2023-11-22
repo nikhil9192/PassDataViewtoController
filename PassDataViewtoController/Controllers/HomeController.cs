9b3e2dc8c3867e6c16b6be8fbf4e774265c22aa3
nikhil9192 <nikhilsinghtech2580@gmail.com>
2023-11-22 09:01:20 +0530

    Add project files.

---
+++ PassDataViewtoController/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PassDataViewtoController.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public   string PostUsingPar(string firstname,string lastname)
+        {
+
+            return "from parameter " + firstname + " " + lastname;
+        }
+
+        [HttpPost]
+        public string PostUsingRequest()
+        {
+
+            string firstname = Request["firstname"];
+            string lastname = Request["lastname"];
+
+
+            return "from Request " + firstname + " " + lastname;
+
+
+
+        }
+    }
+}
---
+++ PassDataViewtoController/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PassDataViewtoController.Global" Language="C#" %>
